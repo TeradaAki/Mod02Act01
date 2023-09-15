ef5c004d9c2f616ad95c9e7a31050619d9addac2
terad <terada.aki@auf.edu.ph>
2023-09-15 23:25:17 +0800

    Add project files.

---
+++ Mod02Act01/Mod02Act01/Model/TaskModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod02Act01.Model
+{
+    class TaskModel
+    {
+        public string Subjectcode { get; set; }
+        public string SubjectTitle { get; set; }
+        public int Unit { get; set; }
+    }
+}
---
+++ Mod02Act01/Mod02Act01/ViewModel/TaskViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Mod02Act01.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mod02Act01.ViewModel
+{
+    internal class TaskViewModel
+    {
+        public TaskModel TaskModelSet { get; set; }
+        public TaskViewModel()
+        {
+            TaskModelSet = new TaskModel()
+            {
+                Subjectcode = "PDC06",
+                SubjectTitle = "Xamarin Mobile Programming",
+                Unit = 3,
+            };
+        }
+    }
+}
